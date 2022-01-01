f7dacdff4278cb2a24faa106c18f064f5b715e22
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2022-01-01 16:33:45 -0500

    asdfdsasadfasdf

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const pool = require('../dbconfig')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  try {
+    const users = pool.query('SELECT * FROM user')
+    res.json(users)
+  } catch (error) {
+    console.error(error)
+  }
 });
 
 module.exports = router;
