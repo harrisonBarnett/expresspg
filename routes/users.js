eca39be579790e2a86fd91ee26b6a0926ecbf61c
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2022-01-01 16:51:18 -0500

    ????

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,22 @@ var router = express.Router();
 const pool = require('../dbconfig')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res) => {
   try {
-    const users = pool.query('SELECT * FROM user')
+    const users = await pool.query('SELECT * FROM users')
     res.json(users)
   } catch (error) {
     console.error(error)
   }
 });
+
+router.post('/', async (req, res) => {
+  try {
+    pool.query('INSERT INTO users (first_name, last_name) VALUES ($1, $2)', [req.body.firstName, req.body.lastName])
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 module.exports = router;
