f1776ca96c179ec479792df4d70b8a736d7358c1
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2022-01-01 20:07:38 -0500

    brief refactor

--- dbconfig.js
+++ dbconfig.js
@@ -12,9 +12,6 @@ var connectionString = 'postgresql://' +
 
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    // ssl: {
-    //     rejectUnauthorized: false
-    // }
     ssl: isProduction ? {rejectUnauthorized: false} : null
 })
 
